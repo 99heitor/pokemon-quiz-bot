9339a92a458fc2f4a75e3eccd3c3d095aafd2f5b
99heitor <99heitor@gmail.com>
2021-03-25 01:15:21 -0300

    Delete messages asynchronously to save on Lambda time

--- pkmnquizbot/commands.go
+++ pkmnquizbot/commands.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -26,11 +27,13 @@ func init() {
 
 //AllPokemon will be initialized by the main function from the csv file
 var allPokemon PokemonList
+var wg sync.WaitGroup
 
 //WhosThatPokemon sends a message with a shadow of a Pokemon image
 func WhosThatPokemon(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	chatId := update.Message.Chat.ID
-	deleteShadowMessage(bot, getGameState(chatId))
+	wg.Add(1)
+	go deleteShadowMessage(bot, getGameState(chatId))
 	log.Printf("Rolling Pokémon for chat %d", chatId)
 	randomPokemon := allPokemon.getRandom()
 	log.Printf("Chose %s for chat %d", randomPokemon.name, chatId)
@@ -39,6 +42,7 @@ func WhosThatPokemon(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	response, _ := bot.Send(msg)
 	log.Printf("Shadow image for %s sent to %d", randomPokemon.name, chatId)
 	saveGameState(chatId, response.MessageID, randomPokemon.id)
+	wg.Wait()
 }
 
 //Its checks if the answer is the one stored for the current chat or is equal to "...", then reveals the answer.
@@ -51,14 +55,17 @@ func Its(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 			msg := tgbotapi.NewMessage(chatId, fmt.Sprintf("[It's %s!](%s)", pokemon.name, pokemon.getPokemonUrl()))
 			msg.ParseMode = "markdown"
 			msg.ReplyToMessageID = update.Message.MessageID
-			deleteShadowMessage(bot, chatConfig)
+			wg.Add(1)
+			go deleteShadowMessage(bot, chatConfig)
 			bot.Send(msg)
+			wg.Wait()
 		}
 	}
 
 }
 
 func deleteShadowMessage(bot *tgbotapi.BotAPI, chatConfig ChatConfig) {
+	defer wg.Done()
 	if chatConfig.ShadowMessageId != 0 {
 		bot.Send(&tgbotapi.DeleteMessageConfig{
 			ChatID:    chatConfig.Id,
